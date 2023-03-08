609c639810bf00b2ecbfa61fc8da074196981dcd
Cassia Carvalho <contato.cassia.carvalho@gmail.com>
2023-03-08 18:55:51 -0300

    att

---
+++ src/TiposVariaveis.java
@@ -0,0 +1,11 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+int a, b;
+
+a = 5;
+b = 6;
+String resultado = a==b ? "verdadeiro" : "falso";
+
+System.out.println(resultado);
+    }
+}
